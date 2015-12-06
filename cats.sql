216600867c0c4254f5623409a909272b1fb8f592
Alex Harris <chasenyc@gmail.com>
2015-12-06 17:45:13 -0500

    Add some associatable tests.

--- cats.sql
+++ cats.sql
@@ -30,7 +30,7 @@ INSERT INTO
 VALUES
   (1, "Alex", "Harris", 1),
   (2, "Jenn", "Diclemente", 1),
-  (3, "Loveless", "Man", 2),
+  (3, "Cody", "Pizzaia", 2),
   (4, "Homeless", "Hobo", NULL);
 
 INSERT INTO
